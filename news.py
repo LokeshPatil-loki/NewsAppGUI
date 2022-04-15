c4929321721c0c2b1182b0a43d07c5073cc0a0ad
LokeshPatil-loki <69594258+LokeshPatil-loki@users.noreply.github.com>
2022-04-15 13:50:30 +0530

    Masked API KEY

--- news.py
+++ news.py
@@ -3,7 +3,7 @@ from newsapi import NewsApiClient
 from NewsModel import NewsModel
 
 def getNews():
-    newsapi = NewsApiClient(api_key='a4c15ce9f82a4a5898474d4c31f6d396')
+    newsapi = NewsApiClient(api_key='ENTER YOUR API KEY HERE')
     top_headlines = newsapi.get_top_headlines(country='in')["articles"]
 
     news_data = list()
